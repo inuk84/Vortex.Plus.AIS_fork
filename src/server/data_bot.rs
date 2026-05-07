166cdefeb9544af9935186b46bf641d466872776
codep1ltio <joonasmikael5@gmail.com>
2026-05-07 14:49:01 +0700

    bot fetching

--- src/server/data_bot.rs
+++ src/server/data_bot.rs
@@ -1,13 +1,33 @@
-use serenity::{
-    async_trait,
-    model::{channel::Message, gateway::Ready, id::ChannelId},
-    prelude::*,
-};
+use serenity::all::ChannelId;
+use serenity::builder::{CreateEmbed, CreateMessage};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
 
-use crate::server::fetch::fetch_user::{self, fetch_client_name};
+use crate::server::fetch::fetch_user::{
+    self, fetch_user_bio, fetch_user_creation, fetch_user_followers, fetch_user_following,
+    fetch_user_friends, fetch_user_status, fetch_user_visits,
+};
 
 fn get_token() -> &'static str {
     "MTUwMTYxNjUzNzQ3ODYyNzQwOA.GTSVrb.zgRDnPJDq-HiY3f1SN60R47R7QxojjmWRwCN6I"
+}
+
+pub async fn send_embed(
+    ctx: &Context,
+    title: &str,
+    desc: &str,
+    content: &str,
+    msg: &Message,
+) -> Result<(), serenity::Error> {
+    let embed = CreateEmbed::new()
+        .title(title)
+        .description(desc)
+        .color(0x00ff00);
+
+    let builder = CreateMessage::new().content(content).embed(embed);
+
+    msg.channel_id.send_message(&ctx.http, builder).await?;
+    Ok(())
 }
 
 async fn send_msg(ctx: &Context, channel_id: u64, content: &str) {
@@ -31,8 +51,8 @@ pub async fn init() {
     impl EventHandler for Handler {
         async fn ready(&self, ctx: Context, ready: Ready) {
             println!("{} is online", ready.user.name);
-            let msg = format!("Client {} Active", fetch_user::fetch_client_name());
-            send_msg(&ctx, LOG_CHANNEL, &msg).await;
+            //let msg = format!("Client {} Active", fetch_user::fetch_client_name());
+            //send_msg(&ctx, LOG_CHANNEL, &msg).await;
         }
 
         async fn message(&self, ctx: Context, msg: Message) {
@@ -41,13 +61,47 @@ pub async fn init() {
             }
 
             let parts: Vec<&str> = msg.content.split_whitespace().collect();
+            let client = reqwest::Client::new();
 
-            if parts.get(0) == Some(&"fetch") {
+            if parts.get(0) == Some(&"help") {
+                let text = format!("
+                **Commands**\n
+                - whois|fetch <id> : finds user info by ID\n
+                ");
+                let _ = msg.channel_id.say(&ctx.http, text).await;
+            }
+
+            if parts.get(0) == Some(&"whoisid") || parts.get(0) == Some(&"fetch") {
                 if let Some(arg) = parts.get(1) {
-                    let text = format!("Attempting to fetch user by id {}", arg);
+                    match fetch_user::fetch_user_name(&client, arg).await {
+                        Some(username) => {
+                            let usr = format!("{}", username);
 
-                    let _ = msg.channel_id.say(&ctx.http, &text).await;
-                    let _ = msg.channel_id.say(&ctx.http, fetch_user::fetch_user(arg)).await;
+                            let desc = format!(
+                                    "{}\n\n**Info**\n\
+                                    id: `{}`\n\
+                                    Status: `{}`\n\
+                                    Creation Date:\n`{}`\n\n\
+                                    **Statistics**\n\
+                                    Friends: `{}`\n\
+                                    Followers: `{}`\n\
+                                    Following: `{}`\n\n\
+                                    Visits: `{}`",
+                                fetch_user_bio(&client, arg).await.unwrap_or("No bio".to_string()), arg,
+                                fetch_user_status(&client, arg).await.unwrap_or("ERROR 404".to_string()),
+                                fetch_user_creation(&client, arg).await.unwrap_or("ERROR 404".to_string()),
+                                fetch_user_friends(&client, arg).await.unwrap_or(0),
+                                fetch_user_followers(&client, arg).await.unwrap_or(0),
+                                fetch_user_following(&client, arg).await.unwrap_or(0),
+                                fetch_user_visits(&client, arg).await.unwrap_or(0),
+                            );
+
+                            let _ = send_embed(&ctx, &usr, &desc, "", &msg).await;
+                        }
+                        None => {
+                            let _ = msg.channel_id.say(&ctx.http, "User not found").await;
+                        }
+                    }
                 }
             }
         }
@@ -55,9 +109,7 @@ pub async fn init() {
 
     let token = get_token();
 
-    let intents =
-        GatewayIntents::GUILD_MESSAGES |
-        GatewayIntents::MESSAGE_CONTENT;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
--- src/server/fetch/fetch_user.rs
+++ src/server/fetch/fetch_user.rs
@@ -1,7 +1,164 @@
+use reqwest::header::{COOKIE};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct User {
+    pub bio: String,
+    pub username: String,
+    pub online_status: String,
+    pub friend_count: u8,
+    pub follower_count: u8,
+    pub following_count: u8,
+    pub visits: u8,
+    pub created_at: String,
+}
+
+static SESSION: &str = "session_token=b23298634b75f5ec18a609882f6b40103620c14c7d9d3e8cfae0a0fe276934bb";
+
 pub fn fetch_client_name() -> String {
     ".".to_string()
 }
 
-pub fn fetch_user(id: &str) -> String {
-    ".".to_string()
+pub async fn fetch_user_name(client: &reqwest::Client, id: &str) -> Option<String> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.username)
+}
+
+pub async fn fetch_user_bio(client: &reqwest::Client, id: &str) -> Option<String> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.bio)
+}
+
+pub async fn fetch_user_status(client: &reqwest::Client, id: &str) -> Option<String> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.online_status)
+}
+
+pub async fn fetch_user_friends(client: &reqwest::Client, id: &str) -> Option<u8> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.friend_count)
+}
+
+pub async fn fetch_user_followers(client: &reqwest::Client, id: &str) -> Option<u8> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.follower_count)
+}
+
+pub async fn fetch_user_following(client: &reqwest::Client, id: &str) -> Option<u8> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.following_count)
+}
+
+pub async fn fetch_user_visits(client: &reqwest::Client, id: &str) -> Option<u8> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.visits)
+}
+
+pub async fn fetch_user_creation(client: &reqwest::Client, id: &str) -> Option<String> {
+    client
+        .get(format!("https://vortex.towerstats.com/api/users/{id}"))
+        .header(
+            COOKIE,
+            SESSION,
+        )
+        .send()
+        .await
+        .ok()?
+        .error_for_status()
+        .ok()?
+        .json::<User>()
+        .await
+        .ok()
+        .map(|u| u.created_at)
 }
